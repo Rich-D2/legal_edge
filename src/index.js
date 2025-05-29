a25af67cd1fcde934de44662a17bcb0ee3d56fc9
Rich-D2 <richi2@gmail.com>
2025-05-29 18:34:32 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { AuthProvider } from "react-oidc-context";
 
-const oidcConfig = {
-  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_PWjircQoX/.well-known/jwks.json",
+const cognitoAuthConfig = {
+  authority: "https://legal-edge.auth.us-east-1.amazoncognito.com", // <-- Hosted UI domain
   client_id: "13gsp3c44n3vt9gtds1q38ggtv",
   redirect_uri: "https://legal-edge.onrender.com",
   response_type: "code",
-  scope: "openid email profile",
+  scope: "email openid phone",
 };
 
 ReactDOM.render(

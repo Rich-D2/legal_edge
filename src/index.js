96720f611c49087691c7a64a90d8272e036c9822
Rich-D2 <richi2@gmail.com>
2025-05-29 18:56:17 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import { AuthProvider } from "react-oidc-context";
 
 const cognitoAuthConfig = {
-  authority: "https://legal-edge.auth.us-east-1.amazoncognito.com",
+  authority: "https://us-east-1pwjircqox.auth.us-east-1.amazoncognito.com",
   client_id: "13gsp3c44n3vt9gtds1q38ggtv",
   redirect_uri: "https://legal-edge.onrender.com",
   response_type: "code",
